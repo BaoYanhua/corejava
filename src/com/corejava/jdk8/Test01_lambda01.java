10809d32973d0ddde5486965e8b64fd958e08d8b
baoyanhua <baoyanhua_coco@163.com>
2017-12-11 00:08:56 +0800

    Test JDK8

---
+++ src/com/corejava/jdk8/Test01_lambda01.java
@@ -0,0 +1,25 @@
+package com.corejava.jdk8;
+
+public class Test01_lambda01 {
+
+	public static void main(String[] args) {
+		new Runnable() {
+			public void run() {
+				System.out.println("inner class implements Runnable interface");
+				System.out.println("cannot refer variable value, only constant value");
+			}
+		}.run();
+		int i=1;
+		Runnable r=()->{
+			System.out.println("Lambda implements Runnable interface");
+			System.out.println("although lambda can refer variable value,"
+					+ " but still cannot change variable value in Runnable method");
+//			i++;---> cannot accept
+			System.out.println("i="+i);
+		};
+		r.run();
+		
+	}
+	
+	
+}
